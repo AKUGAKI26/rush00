6e3eb4d050524fb84004583a45c39d3f6f88acb0
AKU26 <aku.g4ki@gmail.com>
2024-06-29 17:09:02 +0100

    update

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+void rush(int i,int j);
+int main()
+{
+rush(5,3);
+rush(5,1);
+rush(1,1);
+rush(1,5);
+rush(4,4);
+
+}
---
+++ rush00.c
@@ -0,0 +1,55 @@
+#include <unistd.h>
+void ft_putchar(char c);
+void ft_nputchar(int nbr,char c){
+while(nbr>0){
+    ft_putchar(c);
+    nbr--;
+}
+}
+void rows(int i){
+
+    ft_putchar('o');
+    if(i==2)
+    {
+        ft_putchar('o');
+    }
+    else if(i>2)
+    {
+        ft_nputchar(i-2,'-');
+        ft_putchar('o');
+    }
+    ft_putchar('\n');
+    
+}
+void middle_rs(int i){
+    
+    ft_putchar('|');
+    if(i==2)
+    {
+                ft_putchar('|');
+    }
+    else if(i>2)
+    {
+        ft_nputchar(i-2,' ');
+        ft_putchar('|');
+    }
+    ft_putchar('\n');
+
+}
+
+void rush(int i,int j){
+    if(i<=0 || j<=0 || i>=2147483647 || j>=2147483647 )
+    {
+    write(1,"ERROR",5);
+    return ;
+    }
+    rows(i);
+    int len = j;
+    while(len>2){
+        middle_rs(i);
+        len--;
+    }
+    if(j>2)
+        rows(i);
+}
+
